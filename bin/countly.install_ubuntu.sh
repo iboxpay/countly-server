88526f986e1700e44d42a10aea890570e5bffcb4
Chasel Li <chasel.gu@gmail.com>
2020-06-12 11:06:28 +0800

    add fs&out db scripts

--- bin/countly.install_ubuntu.sh
+++ bin/countly.install_ubuntu.sh
@@ -94,12 +94,12 @@ cp -n "$DIR/../frontend/express/public/javascripts/countly/countly.config.sample
 #create api configuration file from sample
 cp -n "$DIR/../api/config.sample.js" "$DIR/../api/config.js"
 
-if [ ! -f $DIR/../api/configs/config.db_out.js ]; then
-	cp $DIR/../api/configs/config.db_out.sample.js $DIR/../api/configs/config.db_out.js
+if [ ! -f "$DIR/../api/configs/config.db_out.js" ]; then
+	cp "$DIR/../api/configs/config.db_out.sample.js" "$DIR/../api/configs/config.db_out.js"
 fi
 
-if [ ! -f $DIR/../api/configs/config.db_fs.js ]; then
-	cp $DIR/../api/configs/config.db_fs.sample.js $DIR/../api/configs/config.db_fs.js
+if [ ! -f "$DIR/../api/configs/config.db_fs.js" ]; then
+	cp "$DIR/../api/configs/config.db_fs.sample.js" "$DIR/../api/configs/config.db_fs.js"
 fi
 
 #create app configuration file from sample
